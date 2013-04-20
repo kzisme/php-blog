9fa3d1deaf96fab0de6750afa1a2aeea27838d7c
Kyle Keiper <goldentoa11@gmail.com>
2013-04-20 14:13:29 -0400

    Aptana Project Now (You don't need to use Aptana to work on it though)
    Implemented TinyMVC class
    Added a bunch of documentation
    Added some to-do tasks
    
    Updated Readme

---
+++ php-blog/system/core/model.php
@@ -0,0 +1,14 @@
+<?php if( !defined("BASE_DIR") ) exit("No Direct Script Access Allowed");
+
+// TO-DO: Document class
+
+class Model {
+	public function user_info()
+	{
+		return array(
+			'first' => 'Jeffery',
+			'last' => 'Way'
+		);
+	}
+
+}
---
+++ php-blog/system/errors/404.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>404 - Document Not Found</title>
+	</head>
+	<body>
+		<div class='wrapper'>
+			<h2>We're Sorry, but the Document You Requested Was Not Found.</h2>
+			<h3>Please Go <a href='<?php echo base_url(); ?>'>Back</a> And Try Something Else.</h3>
+		</div>
+	</body>
+</html>
